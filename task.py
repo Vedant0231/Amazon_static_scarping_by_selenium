ade46a3b27b52ad28ad31d3b93d6466f25f85524
Vedant chaudhari <vedant.chaudhari.777@gmail.com>
2023-01-12 18:11:19 +0530

    file added

--- task.py
+++ task.py
@@ -2,42 +2,50 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-def launchBrowser(phone_name):
+"""user give phone brand name as a input"""
+name = input("enter phone brand name:  ")
 
+"""chrome for testing"""
+driver = webdriver.Chrome()
 
-   driver = webdriver.Chrome()
-   source = f"https://www.amazon.in/s?k={phone_name}&crid=1PO2HKUPJL10Y&sprefix=sa%2Caps%2C1050&ref=nb_sb_ss_ts-doa-p_1_2"
-   driver.get(source)
+"""create dynamic url by the help of user input"""
+source = f"https://www.amazon.in/s?k={name}&crid=1PO2HKUPJL10Y&sprefix=sa%2Caps%2C1050&ref=nb_sb_ss_ts-doa-p_1_2"
 
-   """to get phone name"""
-   phones_name = driver.find_elements(By.XPATH,'//span[@class="a-size-medium a-color-base a-text-normal"]')
-   
-   for value in phones_name:
+driver.get(source)
+
+"""empty list to store dictionaries"""
+final_data =[]
+
+"""to get phone object by this"""
+
+phones = driver.find_elements(By.XPATH,"//div[@class ='s-card-container s-overflow-hidden aok-relative puis-include-content-margin puis s-latency-cf-section s-card-border']")
+
+print(f"here are some of {name} phones for you" )
+
+print(len(phones))
+
+for pr in phones:
     
-    phone_names = value.text
+    """to get phone name """
+    phonename =  pr.find_element(By.TAG_NAME, "h2")
+    
+    """to get phone price"""
+    phoneprice = pr.find_element(By.CLASS_NAME, 'a-price-whole')
 
-    print(phone_names)
+    """store data in dictionary"""
+    final_data.append({
+        "phone_name":phonename.text,
+        "phone_price":phoneprice.text
+    })
 
-   """to get phone price"""
-   phones_price = driver.find_elements(By.XPATH, '//span[@class ="a-price-whole"]')
+for data in final_data:
 
-   for value in phones_price:
+    print(data)
 
-    phone_prices= value.text
+while True:
 
-    print(phone_prices)
-
-   while(True):
-   
-       pass
-
-launchBrowser("samsung")
+    pass
 
 
-"""xpaths"""
-# //span[@class="a-size-medium a-color-base a-text-normal"] phone name
 
-#//span[@class ="a-price-whole"]  phone price
-
-#//div[@class = "sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16"]  phones object 
  
